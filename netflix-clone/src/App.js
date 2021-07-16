f3cc4d3d32d04c354e05ee3faf8531884ad6b0cf
percius47 <33738242+percius47@users.noreply.github.com>
2021-07-16 22:02:43 +0530

    login-full profile-half done

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -7,26 +7,34 @@ import{
     Route
 }from "react-router-dom";
 
-
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from './screens/LoginScreen';
 import { auth } from './firebase';
-function App() {
-    const user=null;
+import {useDispatch, useSelector} from "react-redux";
+import {logout, login, selectUser} from "./features/userSlice";
+import ProfileScreen from './screens/ProfileScreen';
 
-    useEffect(() => {
+function App() {
+    const user=useSelector(selectUser);
+    const dispatch = useDispatch();
+    useEffect(() => { 
        const unsubscribe= auth.onAuthStateChanged(userAuth=>{
         if(userAuth){
             //loggedin
-            console.log(userAuth);
+           
+            dispatch(login({
+                uid:userAuth.uid,
+                email: userAuth.email,
+            }));
         }
         else{
             //loggedout
+            dispatch(logout());
         }
         
     });
         return unsubscribe;
-    }, [])
+    }, [dispatch])
 
   return (
    <div className="app">
@@ -37,6 +45,10 @@ function App() {
     
     (
         <Switch>
+            <Route path="/profile">
+                <ProfileScreen/>
+
+            </Route>
     <Route exact path="/">
     <HomeScreen/>
     </Route>
--- netflix-clone/src/Components/Nav.js
+++ netflix-clone/src/Components/Nav.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import "../Components/Nav.css";
 import Logo from '../images/netflix.png'
@@ -6,7 +7,7 @@ import Logo from '../images/netflix.png'
 function Nav() {
 
     const [show,setShow]=useState(false);
-
+    const history=useHistory();
     const transitionNavBar =()=>{
         if(window.scrollY>100){
             setShow(true);
@@ -28,9 +29,13 @@ function Nav() {
 
             <div className="nav_contents">
 
-            <img className="nav_logo" src={Logo} alt="Netflix"/>
+            <img 
+            onClick={()=>history.push("/")}
+            className="nav_logo" src={Logo} alt="Netflix"/>
 
-        <img className="nav_avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Avatar"/>
+        <img 
+        onClick={()=>history.push("/profile")}
+        className="nav_avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Avatar"/>
             </div>
            
         </div>
